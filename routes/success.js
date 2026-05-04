cb6c2c282a02e60b47f50ca0d039d661d5e35be6
mind-loop <purevbaasankhuu@gmail.com>
2026-05-04 20:58:07 +0700

    dxs

--- routes/success.js
+++ routes/success.js
@@ -3,7 +3,7 @@ const router = express.Router();
 router.route("/").get((req, res) => {
   res.status(200).json({
     message: {
-      version: "v1.0.1",
+      version: "v1.0.2",
       message: "LAST UPDATE: CDN EMAIL APP PASSWORD CHANGED",
       points: [
         "1. File upload",
